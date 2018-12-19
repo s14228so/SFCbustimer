20a99b21aa271ab6162959d6322d2317bc7358ac
satoshi ono <saongtx7@gmail.com>
2018-12-19 21:36:34 +0900

    init

--- Dotinstall/ViewController/SettingViewController.swift
+++ Dotinstall/ViewController/SettingViewController.swift
@@ -1,25 +1,31 @@
 //
-//  ViewController.swift
+//  SettingViewController.swift
 //  Dotinstall
 //
-//  Created by satoshi on 2018/11/22.
+//  Created by satoshi on 2018/12/17.
 //  Copyright © 2018年 satoshi. All rights reserved.
 //
 
 import UIKit
+//import RAMPaperSwitch
 
-class ViewController: UIViewController {
+
+class SettingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
-
+    
+    @IBAction func `switch`(_ sender: Any) {
+        
+    }
+    
 
 }
-
---
+++ Pods/Target Support Files/Pods-Dotinstall/Pods-Dotinstall-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dotinstall : NSObject
+@end
+@implementation PodsDummy_Pods_Dotinstall
+@end
